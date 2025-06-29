e3a2ec89773744e3d6adecf7aff6a65f42dea3cb
navaneethhhk <hknavaneeth@gmail.com>
2025-06-29 12:59:46 +0530

    2906

--- src/test/java/org/example/sample/TestApp.java
+++ src/test/java/org/example/sample/TestApp.java
@@ -1,4 +1,12 @@
 package org.example.sample;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class TestApp {
+    public static void main (String[] args){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/in/navaneeth-hk");
+    }
 }
--- src/test/java/org/example/tests/BasicCommands.java
+++ src/test/java/org/example/tests/BasicCommands.java
@@ -1,4 +1,31 @@
 package org.example.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 public class BasicCommands {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.applitools.com");
+        String title=  driver.getTitle();
+        String url=driver.getCurrentUrl();
+        String page=driver.getPageSource();
+        Assert.assertEquals(url,"https://demo.applitools.com/");
+        Assert.assertEquals(title,"ACME Demo App by Applitools");
+        Assert.assertTrue(page.contains("Login Form"));
+        driver.manage().window().minimize();
+        driver.navigate().to("https://www.google.com");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.manage().window().fullscreen();
+        driver.navigate().refresh();
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.close();
+    }
 }
