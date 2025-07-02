fa121f97f3679bd63bdd6a102e9bd57c87798b73
navaneethhhk <hknavaneeth@gmail.com>
2025-07-02 07:39:31 +0530

    0207

--- src/test/java/org/example/tests/Ex02072025/ShadowDOM.java
+++ src/test/java/org/example/tests/Ex02072025/ShadowDOM.java
@@ -1,5 +1,29 @@
 package org.example.tests.Ex02072025;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
 public class ShadowDOM {
+    @Test
+    public void shadowdom(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://selectorshub.com/xpath-practice-page/");
+        JavascriptExecutor js =(JavascriptExecutor) driver;
+        WebElement scroll = driver.findElement(By.xpath("//div[@id='userName']"));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        WebElement inputPizza = (WebElement) js.executeScript("return document.querySelector(\"div#userName\").shadowRoot.querySelector(\"div#app2\").shadowRoot.querySelector(\"#pizza\");");
+         inputPizza.sendKeys("margarita");
+    }
 
 }
