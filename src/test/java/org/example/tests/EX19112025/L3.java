19687030d23467e9e40ab9a40d6e4cb5564149fc
navaneethhhk <hknavaneeth@gmail.com>
2025-11-19 08:40:26 +0530

    1911

--- src/test/java/org/example/tests/EX19112025/L3.java
+++ src/test/java/org/example/tests/EX19112025/L3.java
@@ -1,8 +1,23 @@
 package org.example.tests.EX19112025;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import  org.openqa.selenium.chrome.ChromeDriver;
+import  org.openqa.selenium.chrome.ChromeOptions;
 public class L3 {
     @Test
     public static void test(){
-
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--incognito");
+        //options.addArguments("Headless");
+        options.addArguments("--window-size=100,500");
+        WebDriver driver= new ChromeDriver(options);
+        driver.get("https://www.linkedin.com");
+        try{
+            Thread.sleep(10);
+        }catch(InterruptedException e){
+            throw new RuntimeException (e);
+        }
+        driver.quit();
     }
 }
