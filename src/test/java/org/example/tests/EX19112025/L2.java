b57eab18ad162633498d840fd8d3fff9dd2d62eb
navaneethhhk <hknavaneeth@gmail.com>
2025-11-20 20:28:43 +0530

    2011

--- src/test/java/org/example/tests/EX19112025/L2.java
+++ src/test/java/org/example/tests/EX19112025/L2.java
@@ -1,11 +1,18 @@
 package org.example.tests.EX19112025;
 
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.WebElement;
 import java.time.Duration;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 public class L2 {
     @Test
@@ -26,5 +33,25 @@ public class L2 {
         driver.get("https://www.google.com");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.quit();
+    }
+
+    @Test
+    public void test3(){
+        WebDriver driver=new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        driver.get("https://www.facebook.com");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='email']"))).sendKeys("nithu");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='pass']"))).sendKeys("123");
+        driver.findElement(By.name("login")).click();
+        driver.quit();
+    }
+    @Test
+    public void test4(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        Wait<WebDriver> wait= new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30)).pollingEvery(Duration.ofSeconds(5)).ignoring(NoSuchElementException.class);
+
+        WebElement element =wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("name")));
+        element.click();
     }
 }
