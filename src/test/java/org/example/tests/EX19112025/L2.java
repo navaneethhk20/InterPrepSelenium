37dfe8c1b570941bb2e0d89481a42dc690651e66
navaneethhhk <hknavaneeth@gmail.com>
2025-11-20 19:16:15 +0530

    2011

---
+++ src/test/java/org/example/tests/EX19112025/L2.java
@@ -0,0 +1,24 @@
+package org.example.tests.EX19112025;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+public class L2 {
+    @Test
+    public static void  test(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com");
+        driver.findElement(By.id("email")).sendKeys("hknavaneeth@gmail.com");
+        driver.findElement(By.name("pass")).sendKeys("nithu123");
+        driver.findElement(By.partialLinkText("Forgotten")).click();
+        //driver.findElement(By.xpath("//button[@type='submit']")).click();
+    }
+    @Test
+    public static void  test2(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("");
+    }
+}
